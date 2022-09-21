a61e8af6f4a6f93dd476f8c712e378e887c90b26
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2022-09-21 20:29:52 -0300

    Exercícios realizados até operadores lógicos

---
+++ 01-Web_Development_Fundamentals/04_JavaScript-Fundamentals/day01-FirstSteps/exercicioFoguete2.js
@@ -0,0 +1,9 @@
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = 2 * base + 2 * height;
+
+console.log(base);
+console.log(height);
+console.log(area);
+console.log(perimeter);
