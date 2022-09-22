1a288500e08f7d0bf6374bb92331cbc192f4c2ec
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2022-09-22 00:23:01 -0300

    Ainda faltam alguns exercícios

---
+++ 01-Web_Development_Fundamentals/04_JavaScript-Fundamentals/day01-FirstSteps/exercicio5_OperacoesAritmeticas.js
@@ -0,0 +1,14 @@
+const a = 37;
+const b = 7;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = Math.round(a / b);
+let resto = a % b;
+
+console.log("Para A = " + a + " e B = " + b + ":");
+console.log("- Soma = " + adicao );
+console.log("- Subtração = " + subtracao);
+console.log("- Multiplicacao = " + multiplicacao);
+console.log("- Divisão = "+ divisao + ", com Resto = " + resto);
---
+++ 01-Web_Development_Fundamentals/04_JavaScript-Fundamentals/day01-FirstSteps/exercicio7-IfElse2.js
@@ -0,0 +1,9 @@
+const a = 10;
+const b = 15;
+const c = 20;
+
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
+} else console.log(c);
