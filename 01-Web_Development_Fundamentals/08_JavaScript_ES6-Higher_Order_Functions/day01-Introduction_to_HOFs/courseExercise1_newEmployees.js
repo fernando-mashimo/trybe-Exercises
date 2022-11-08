5f96a81840e1bb2c2456240e1e316d66603c9b65
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2022-11-08 15:33:00 -0300

    Subindo últimos arquivos

--- 01-Web_Development_Fundamentals/08_JavaScript_ES6-Higher_Order_Functions/day01-Introduction_to_HOFs/courseExercise1_newEmployees.js
+++ 01-Web_Development_Fundamentals/08_JavaScript_ES6-Higher_Order_Functions/day01-Introduction_to_HOFs/courseExercise1_newEmployees.js
@@ -1,22 +1,16 @@
 // Nova pessoa contratada
 // Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. A sua função deve receber como parâmetro o nome completo da pessoa funcionária e, a partir dele, gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
 
-const newEmployees = (callback) => {
-    const employees = {
+const newEmployees = (callback) => ({
       id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
       id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
       id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    }
-    return employees;
-  };
+    });
 
-const newData = (fullName) => {
   //retorna objeto { nomeCompleto, email }
-  const employeeData = {
+const newData = (fullName) => ({
     nome:   fullName,
     email:  `${fullName.replace(/\s+/g, '_').toLowerCase()}@trybe.com`,
-  }
-  return employeeData;
-};
+  });
 
 console.log(newEmployees(newData));
