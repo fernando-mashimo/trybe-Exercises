938bdedef6d34825cff9fa2cec78137e1e27c5b1
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2022-11-23 09:14:29 -0300

    Adicionando exercicios Course do dia

---
+++ 02-Front-End_Development/02_Introduction_to_React/day01-Hello_World/helloworld/src/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class MySkills extends React.Component {
+    render() {
+      return (
+        <>
+        <h1>Fernando</h1>
+        <p>Hello world!</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>React</li>
+        </ul>
+        </>
+      )
+    }
+}
+
+export default MySkills;
